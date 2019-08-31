452659e8ac5d4d112239878905119d38dddc4446
Dawood <dawood.aijaz97@gmail.com>
2019-08-31 18:18:05 +0500

    #

--- scapy_attack.py
+++ scapy_attack.py
@@ -2,5 +2,9 @@ from scapy.all import *
 
 p = (IP(dst="144.217.100.106") / TCP(dport=80, flags="S"))
 syn_ack = sr1(p)
-print(type(syn_ack))
 print(str(syn_ack.__repr__()))
+getStr = 'GET / HTTP/1.1\r\nHost: www.mehrangroup.com\r\n\r\n'
+
+request = IP(dst="144.217.100.106") / TCP(dport=80, sport=syn_ack[TCP].dport,seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='A') / getStr
+reply = sr1(p)
+print(reply.__repr__())
