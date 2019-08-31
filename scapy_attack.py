111c1fe8ebc57d247250a74dc3cf84a491d9f564
Dawood <dawood.aijaz97@gmail.com>
2019-08-31 19:13:40 +0500

    #

--- scapy_attack.py
+++ scapy_attack.py
@@ -1,19 +1,21 @@
 from scapy.all import *
-
+import random
 
 seq = 12345
 dest = "35.193.17.254"
 sport = 1040
 dport = 8080
 syn = IP(dst=dest) / TCP(dport=80, flags='S')
+max = 20
+counter = 0
+while counter < max:
 
-# GET SYNACK
-syn_ack = sr1(syn)
-# Send ACK
-out_ack = send(
-    IP(dst=dest) / TCP(dport=80, sport=syn_ack[TCP].dport, seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='A'))
+    syn = IP(dst=dest) / TCP(sport=random.randint(1025,65500), dport=80, flags='S')
 
-# Send the HTTP GET
-getStr = 'GET / HTTP/1.1\r\nHost:' + dest + '\r\nAccept-Encoding: gzip, deflate\r\n\r\n'
-reply = sr1(IP(dst=dest) / TCP(dport=80, sport=syn_ack[TCP].dport, seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1,flags='P''A') / getStr)
-print(reply.__repr__())
+    syn_ack = sr1(syn)
+    getStr = 'GET / HTTP/1.1\r\nHost:' + dest + '\r\nAccept-Encoding: gzip, deflate\r\n\r\n'
+
+    out_ack = send(IP(dst=dest) / TCP(dport=80, sport=syn_ack[TCP].dport,seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='A'))
+
+    sr1(IP(dst=dest) / TCP(dport=80, sport=syn_ack[TCP].dport,seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='P''A') / getStr)
+    counter += 1
