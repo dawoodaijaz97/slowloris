b2705ea312b8a5e0a7a8a5a2c887a7c4022d4db2
Dawood <dawood.aijaz97@gmail.com>
2019-08-31 20:02:28 +0500

    #

--- scapy_attack.py
+++ scapy_attack.py
@@ -1,9 +1,10 @@
 from scapy.all import *
 import random
 
-x = "35.193.17.254"
-y =x
-request = "GET / HTTP/1.1\r\nHost: " + y + "\r\n"
+x = "instance2mymachines.xyz"
+y = "www.instance2mymachines.xyz"
+
+request = "GET / HTTP/1.1\r\nHost: " + y + "\r\n\r\n"
 p = IP(dst=x) / TCP() / request
 out = sr1(p)
 if out:
