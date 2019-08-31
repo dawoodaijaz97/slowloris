41e81d5b8e0ee198967268f6aeb09cec510aa8fd
Dawood <dawood.aijaz97@gmail.com>
2019-08-31 19:44:30 +0500

    #

--- scapy_attack.py
+++ scapy_attack.py
@@ -11,4 +11,7 @@ syn_ack = sr1(syn)
 
 out_ack = send(IP(dst=dest) / TCP(dport=80, sport=syn_ack[TCP].dport, seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='A'))
 getStr = 'GET / HTTP/1.1\r\nHost:' + dest + '\r\nAccept-Encoding: gzip, deflate\r\n\r\n'
-sr1(IP(dst=dest) / TCP(dport=80, sport=syn_ack[TCP].dport, seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1,flags='P''A') / getStr)
+reply = sr1(IP(dst=dest) / TCP(dport=80, sport=syn_ack[TCP].dport, seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1,flags=['P','A']) / getStr)
+
+
+print(reply.__repr__())
