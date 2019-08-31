e6af03937d5af8e704cf94fe6b0dd69a18f7538f
Dawood <dawood.aijaz97@gmail.com>
2019-08-31 19:24:34 +0500

    #

--- scapy_attack.py
+++ scapy_attack.py
@@ -1,21 +1,19 @@
 from scapy.all import *
 import random
 
-seq = 12345
+
 dest = "35.193.17.254"
-sport = 1040
-dport = 80
-syn = IP(dst=dest) / TCP(dport=80, flags='S')
-max = 20
-counter = 0
-while counter < max:
 
-    syn = IP(dst=dest) / TCP(sport=random.randint(1025,65500), dport=80, flags='S')
-
-    syn_ack = sr1(syn)
-    getStr = 'GET / HTTP/1.1\r\nHost:' + dest + '\r\nAccept-Encoding: gzip, deflate\r\n\r\n'
-
-    out_ack = send(IP(dst=dest) / TCP(dport=80, sport=syn_ack[TCP].dport,seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='A'))
-
-    sr1(IP(dst=dest) / TCP(dport=80, sport=syn_ack[TCP].dport,seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='P''A') / getStr)
-    counter += 1
+load_layer("http")
+req = HTTP()/HTTPRequest(
+    Accept_Encoding=b'gzip, deflate',
+    Cache_Control=b'no-cache',
+    Connection=b'keep-alive',
+    Host=b'www.secdev.org',
+    Pragma=b'no-cache'
+)
+a = TCP_client.tcplink(HTTP,"35.193.17.254", 80)
+answser = a.sr1(req)
+a.close()
+with open("www.secdev.org.html", "wb") as file:
+    file.write(answser.load)
