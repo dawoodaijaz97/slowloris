fc624f86c37ba44c577b277412b317ab34e600d9
Dawood <dawood.aijaz97@gmail.com>
2019-08-31 18:39:21 +0500

    #

--- scapy_attack.py
+++ scapy_attack.py
@@ -1,10 +1,26 @@
 from scapy.all import *
 
-p = (IP(dst="144.217.100.106") / TCP(dport=80, flags="S"))
-syn_ack = sr1(p)
-print(str(syn_ack.__repr__()))
-getStr = 'GET / HTTP/1.1\r\n\r\n'
+seq = 12345
+sport = 1040
+dport = 80
 
-request = IP(dst="144.217.100.106") / TCP(dport=80, sport=syn_ack[TCP].dport,seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='A') / getStr
-reply = sr1(p)
-print(reply.__repr__())
+ip_packet = IP(dst='144.217.100.106')
+syn_packet = TCP(sport=sport, dport=dport, flags='S', seq=seq)
+
+packet = ip_packet/syn_packet
+synack_response = sr1(packet)
+
+next_seq = seq + 1
+my_ack = synack_response.seq + 1
+
+ack_packet = TCP(sport=sport, dport=dport, flags='A', seq=next_seq, ack=my_ack)
+
+send(ip_packet/ack_packet)
+
+payload_packet = TCP(sport=sport, dport=dport, flags='A', seq=next_seq, ack=my_ack)
+payload = "GET / HTTP/1.0\r\nHOST: 144.217.100.106\r\n\r\n"
+
+reply, error = sr(ip_packet/payload_packet/payload, multi=1, timeout=1)
+for r in reply:
+    r[0].show2()
+    r[1].show2()
