fb1df0ad6fa4e921f21834e46cb6f2bf28b53064
Dawood <dawood.aijaz97@gmail.com>
2019-08-31 19:29:12 +0500

    #

--- scapy_attack.py
+++ scapy_attack.py
@@ -9,7 +9,7 @@ req = HTTP()/HTTPRequest(
     Accept_Encoding=b'gzip, deflate',
     Cache_Control=b'no-cache',
     Connection=b'keep-alive',
-    Host=b'www.secdev.org',
+    Host=b'35.193.17.254',
     Pragma=b'no-cache'
 )
 a = TCP_client.tcplink(HTTP,"35.193.17.254", 8080)
