49a73ff06b66c7df337e24af19e18b97a9d73dcb
Dawood <dawood.aijaz97@gmail.com>
2019-08-31 19:59:03 +0500

    #

--- scapy_attack.py
+++ scapy_attack.py
@@ -1,17 +1,10 @@
 from scapy.all import *
 import random
 
-dest = "35.193.17.254"
-user_agent = "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
-
-syn = IP(dst=dest) / TCP(dport=80, flags='S')
-
-syn_ack = sr1(syn)
-
-out_ack = send(
-    IP(dst=dest) / TCP(dport=80, sport=syn_ack[TCP].dport, seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='A'))
-getStr = f'GET / HTTP/1.1\r\nHost:{dest}\r\nAccept-Encoding: gzip, deflate\r\nUser-agent:{user_agent}\r\n\r\n'
-reply = sr1(IP(dst=dest) / TCP(dport=80, sport=syn_ack[TCP].dport, seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1,
-                               flags=['P', 'A']) / getStr)
-
-print(reply.__repr__())
+x = "35.193.17.254"
+y =x
+request = "GET / HTTP/1.1\r\nHost: " + y + "\r\n"
+p = IP(dst=x) / TCP() / request
+out = sr1(p)
+if out:
+    out.show()
