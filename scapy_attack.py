cf6c0f3166ef6f803b5bcfea62e1e1edb751ea3f
Dawood <dawood.aijaz97@gmail.com>
2019-08-31 18:58:36 +0500

    #

--- scapy_attack.py
+++ scapy_attack.py
@@ -2,13 +2,15 @@ from scapy.all import *
 
 seq = 12345
 sport = 1040
-dport = 80
+dport = 8080
 
 ip_packet = IP(dst='35.193.17.254')
-syn_packet = TCP(sport=sport, dport=dport, flags='S', seq=seq)
+syn_packet = TCP(sport=sport, dport=dport, flags='S')
 
 packet = ip_packet/syn_packet
 synack_response = sr1(packet)
+
+print(synack_response.__repr__())
 
 next_seq = seq + 1
 my_ack = synack_response.seq + 1
@@ -18,7 +20,7 @@ ack_packet = TCP(sport=sport, dport=dport, flags='A', seq=next_seq, ack=my_ack)
 send(ip_packet/ack_packet)
 
 payload_packet = TCP(sport=sport, dport=dport, flags='A', seq=next_seq, ack=my_ack)
-payload = "GET / HTTP/1.0\r\nHOST: 35.193.17.254\r\n\r\n"
+payload = "GET / HTTP/1.0 \r\n\r\n"
 
 reply, error = sr(ip_packet/payload_packet/payload, multi=1, timeout=1)
 for r in reply:
