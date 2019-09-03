5010d729bf54ce393f4b3bcd392edb6abf717444
Dawood <dawood.aijaz97@gmail.com>
2019-09-03 20:27:35 +0500

    #

--- scapy_attack.py
+++ scapy_attack.py
@@ -6,8 +6,8 @@ y = "www.instance2mymachines.xyz"
 ip = "35.193.17.254"
 dport = 8080
 
-request = "GET / HTTP/1.1\r\nHost: " + y + "\r\nUser-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1\r\n\r\n"
-p = IP(dst=ip) / TCP() / request
+request = "GET / HTTP/1.1\r\nHost: " + y + ":8080\r\nUser-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1\r\n\r\n"
+p = IP(dst=ip) / TCP(dport=dport) / request
 out = sr1(p)
 if out:
     out.show()
