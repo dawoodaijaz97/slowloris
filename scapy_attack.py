228011d51401f423cc4302ec31857af801f97b1f
Dawood <dawood.aijaz97@gmail.com>
2019-09-03 19:40:47 +0500

    #

--- scapy_attack.py
+++ scapy_attack.py
@@ -4,7 +4,7 @@ import random
 x = "instance2mymachines.xyz"
 y = "www.instance2mymachines.xyz"
 
-syn = IP(dst='www.google.com') / TCP(dport=80, flags='S')
+syn = IP(dst=y) / TCP(dport=8080, flags='S')
 
 print(syn.__repr__())
 
@@ -12,8 +12,8 @@ syn_ack = sr1(syn)
 
 print(syn_ack.__repr__())
 
-getStr = 'GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n'
-request = IP(dst='www.google.com') / TCP(dport=80, sport=syn_ack[TCP].dport,seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='A') / getStr
+getStr = 'GET / HTTP/1.1\r\nHost: www.instance2mymachines.xyz\r\n\r\n'
+request = IP(dst=y) / TCP(dport=8080, sport=syn_ack[TCP].dport,seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='A') / getStr
 
 reply = sr1(request)
 
