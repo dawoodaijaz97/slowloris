b4c2bb0e325239acdf2076a4c7836b9631904113
Dawood <dawood.aijaz97@gmail.com>
2019-09-03 20:09:45 +0500

    #

--- scapy_attack.py
+++ scapy_attack.py
@@ -4,20 +4,10 @@ from random import randint
 x = "instance2mymachines.xyz"
 y = "www.instance2mymachines.xyz"
 ip = "35.193.17.254"
-
-sport = int(randint(1025, 65535))
 dport = 8080
-seq = randint(0, 110000)
 
-syn = IP(dst=ip) / TCP(dport=dport, sport=sport, flags='S', seq=seq)
-synack = sr1(syn)
-
-print(synack.summary())
-
-seq = synack[TCP].ack
-ack = synack[TCP].seq + 1
-
-payload = "GET / HTTP/1.1\r\nHost: " + y + "\r\n\r\n"
-request = IP(dst=ip) / TCP(dport=dport, sport=sport, flags='A', seq=seq, ack=ack) / payload
-ans, unans = sr(request)
-ans.summary()
+request = "GET / HTTP/1.1\r\nHost: " + y + "\r\n\r\n"
+p = IP(dst=ip) / TCP() / request
+out = sr1(p)
+if out:
+    out.show()
