5d787822cf42608fb8f29fa22af945f2621354de
Dawood <dawood.aijaz97@gmail.com>
2019-09-03 19:56:40 +0500

    #

--- scapy_attack.py
+++ scapy_attack.py
@@ -8,14 +8,14 @@ syn = IP(dst=y) / TCP(dport=8080, flags='S')
 
 print(syn.__repr__())
 
-syn_ack = sr(syn)
+syn_ack = sr1(syn)
 
 print(syn_ack.__repr__())
 
 getStr = 'GET / HTTP/1.1\r\nHost: www.instance2mymachines.xyz\r\n\r\n'
 request = IP(dst=y) / TCP(dport=8080, sport=syn_ack[TCP].dport,seq=syn_ack[TCP].ack, ack=syn_ack[TCP].seq + 1, flags='A') / getStr
 
-reply,unans = sr(request)
+reply,unans = sr1(request)
 
 reply.summary()
 
