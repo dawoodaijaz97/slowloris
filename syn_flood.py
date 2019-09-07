6413df02849620309492a4829e0cf1a20eb8c25d
Dawood <dawood.aijaz97@gmail.com>
2019-09-07 18:49:10 +0500

    #

--- syn_flood.py
+++ syn_flood.py
@@ -11,14 +11,25 @@ port = 80
 
 
 def attack():
-    block1 = str(random.randint(10, 127))
-    block2 = str(random.randint(10, 255))
-    block3 = str(random.randint(10, 255))
-    block4 = str(random.randint(10, 255))
+    block1 = ''
+    block2 = ''
+    block3 = ''
+    block4 = ''
+    while True:
+        block1 = str(random.randint(10, 127))
+        block2 = str(random.randint(10, 255))
+        block3 = str(random.randint(10, 255))
+        block4 = str(random.randint(10, 255))
+        if block1 != "10" and block1 != "172" and block1 != "192":
+            break
 
     ip = block1 + "." + block2 + "." + block3 + "." + block4 + "\\8"
+    print(f"Network:{ip}")
 
-    print(ip)
+    ip_network = ipaddress.ip_network(ip)
+
+    for ip in ip_network:
+        print(ip.__str__())
 
 
 for i in range(0, no_of_process):
